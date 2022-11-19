00c1119687b83af3875e19ee1722b3476bef58fb
Ádám Várhegyi-Miłoś <avarhegyi87@gmail.com>
2022-11-19 21:48:46 +0100

    fixed collapsed navbar for Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from './models';
 import { AuthenticationService } from './services';
@@ -6,14 +6,25 @@ import { AuthenticationService } from './services';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  name = 'Angular ' + VERSION.major;
   title = 'ITProvisioning.UI';
   currentUser: User | undefined;
+  navbarCollapsed = true;
 
-  constructor(private router: Router, private authenticationService: AuthenticationService) {
-    this.authenticationService.getCurrentUser.subscribe(x => this.currentUser = x);
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.getCurrentUser.subscribe(
+      (x) => (this.currentUser = x)
+    );
+  }
+
+  toggleNavbarCollapsing() {
+    this.navbarCollapsed = !this.navbarCollapsed;
   }
 
   logout() {
